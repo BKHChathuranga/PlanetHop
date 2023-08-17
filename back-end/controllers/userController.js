ae5397ca3bdc1a9069f58fc2dc27ca87aa21254f
Thilina1999 <w.thilinamalshanfernando@gmail.com>
2023-08-17 16:15:25 +0530

    backend: user login function

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,35 +1,60 @@
-const User = require('../models/user');
-const response = require('../utils/response');
-const bcrypt = require('bcrypt');
-const logger = require('../utils/logger');
+const User = require("../models/user");
+const response = require("../utils/response");
+const bcrypt = require("bcrypt");
+const logger = require("../utils/logger");
 
 exports.register = async (req, res) => {
-    try {
-        const { firstName, lastName, npi, email, password, phoneNumber } = req.body;
-        const existingUser = await User.findOne({ $or: [{ npi }, { email }] });
+  try {
+    const { firstName, lastName, npi, email, password, phoneNumber } = req.body;
+    const existingUser = await User.findOne({ $or: [{ npi }, { email }] });
 
-        if (existingUser) {
-            logger.warn('NPI or email already exists');
-            return response.response(res, 'NPI or email already exists', null, 400);
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const newUser = new User({
-            firstName,
-            lastName,
-            npi,
-            email,
-            password: hashedPassword,
-            phoneNumber
-        });
-
-        await newUser.save();
-        
-        logger.info('User registered successfully');
-        return response.response(res, 'User registered successfully', newUser, 201);
-    } catch (error) {
-        logger.error("Error while registering user", error.message);
-        return response.response(res, error.message);
+    if (existingUser) {
+      logger.warn("NPI or email already exists");
+      return response.response(res, "NPI or email already exists", null, 400);
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      npi,
+      email,
+      password: hashedPassword,
+      phoneNumber,
+    });
+
+    await newUser.save();
+
+    logger.info("User registered successfully");
+    return response.response(res, "User registered successfully", newUser, 201);
+  } catch (error) {
+    logger.error("Error while registering user", error.message);
+    return response.response(res, error.message);
+  }
+};
+
+exports.userLogin = async ({ body: { email, password } }, res) => {
+  try {
+    if (!email || !password) {
+      logger.warn("email or Password empty");
+      response.response(res, "Please enter email and password", null, 400);
+    }
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      logger.warn("email is not registered");
+      return response.response(res, "User not found", null, 400);
+    }
+
+    if (!(await bcrypt.compareSync(password, user.password))) {
+      logger.warn("Password is Incorrect");
+      return response.response(res, "Password is Incorrect", null, 400);
+    }
+
+    return response.response(res, "User found", null, 200);
+  } catch (error) {
+    logger.error("Error while logging in user", error.message);
+    return response.response(res, error.message, null, 400);
+  }
 };
--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -4,5 +4,6 @@ const router = app.Router();
 const userController = require('../controllers/userController');
 
 router.post('/register', userController.register);
+router.post('/login', userController.userLogin);
 
 module.exports = router;
