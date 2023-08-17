261ae5b085be4ea23b65738c97c3d2ae5a7bf175
ihakalanka <akalankaih19@gmail.com>
2023-08-18 01:01:01 +0530

    added payment controller functionality

---
+++ back-end/controllers/paymentController.js
@@ -0,0 +1,41 @@
+const logger = require('../utils/logger');
+const { response } = require('../utils/response');
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+
+exports.createPayment = async (req, res) => {
+    try {
+        const { from, to, transportationMode, email, cost } = req.body;
+        const totalAmount = cost * 100;
+
+        const customer = await stripe.customers.create({
+            email: email,
+        });
+
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            mode: 'payment',
+            line_items: [
+                {
+                    price_data: {
+                        currency: 'usd',
+                        unit_amount: totalAmount,
+                        product_data: {
+                            name: 'Travelling Cost',
+                            description: `Travel details: From ${from} to ${to} via ${transportationMode}.`,
+                        },
+                    },
+                    quantity: 1,
+                },
+            ],
+            success_url: `http://localhost:3000/success`,
+            cancel_url: `http://localhost:3000/cancel`,
+            customer: customer.id,
+        })
+
+        response.response(res, "Payment created successfully", session.url, 201);
+    }catch (error){
+        logger.error("Error while creating payment", error.message);
+        return response.response(res, "Error while creating payment", null, 400);
+    }
+};
