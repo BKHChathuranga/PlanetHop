d041c9b83833def1d902e54d612d6b842e4a9891
Nipuna Bhanuka <nipunahettiwatta@gamil.com>
2023-08-17 19:09:45 +0530

    fetch locations

--- back-end/routes/locationRoutes.js
+++ back-end/routes/locationRoutes.js
@@ -4,6 +4,5 @@ const router = app.Router();
 const locationController = require('../controllers/locationController');
 
 router.get('/location', locationController.fetchlocations);
-router.post('/location', locationController.postlocations);
 
 module.exports = router;
