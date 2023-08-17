9f1adfbee100134497fb2f8f31cd12c9ff1f451b
Thilina1999 <w.thilinamalshanfernando@gmail.com>
2023-08-17 11:21:45 +0530

    backend: database initiate

--- back-end/server.js
+++ back-end/server.js
@@ -5,8 +5,20 @@ const cors = require('cors');
 
 
 const app = express();
+require('dotenv').config();
+const port = parseInt(process.env.PORT);
+mongoose.set("strictQuery", false);
 
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('Connected to mongodb')).catch(e => console.log('Error occured when connecting to mongodb', e));
 
+//Body parsing as JSON
+app.use(bodyParser.json());
+
+//Enable cors
+app.use(cors());
 app.get('/', (req, res) => {
     return res.status(200).json({
         status: 200,
@@ -14,6 +26,6 @@ app.get('/', (req, res) => {
     });
 });
 
-app.listen(5000, () => {
-    console.log('Server running on port 5000');
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
 });
