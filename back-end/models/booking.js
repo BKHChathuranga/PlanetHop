6ec291067ec1a93784132211000dbc03c18c87b3
ihakalanka <akalankaih19@gmail.com>
2023-08-17 20:25:34 +0530

    added booking model

---
+++ back-end/models/booking.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+    date: {
+        type: Date,
+        required: [true, 'Date is required']
+    },
+    totalPrice: {
+        type: Number,
+        required: [true, 'Total price is required']
+    },
+    transportationMode: {
+        type: String,
+        required: [true, 'Transportation mode is required']
+    },
+    from: {
+        type: String,
+        required: [true, 'From is required']
+    },
+    to: {
+        type: String,
+        required: [true, 'To is required']
+    },
+    departureTime: {
+        type: Date
+    }
+});
+
+module.exports = mongoose.model('Booking', bookingSchema);
