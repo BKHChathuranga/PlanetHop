688bf199687ee2460fe93e6b0245d040c7e58038
ihakalanka <akalankaih19@gmail.com>
2023-08-19 15:40:35 +0530

    backend: added payment & transportation logic

---
+++ back-end/controllers/paymentController.js
@@ -0,0 +1,38 @@
+const logger = require('../utils/logger');
+const response = require('../utils/response');
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+
+exports.createPayment = async (req, res) => {
+    try {
+        const { from, to, transportationMode, email, cost } = req.body;
+        const totalAmount = cost * 100;
+
+        const customer = await stripe.customers.create({ email });
+
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            mode: 'payment',
+            line_items: [
+                {
+                    price_data: {
+                        currency: 'usd',
+                        unit_amount: totalAmount,
+                        product_data: {
+                            name: 'Travelling Cost',
+                            description: `Travel details: From ${from} to ${to} via ${transportationMode}.`,
+                        },
+                    },
+                    quantity: 1,
+                },
+            ],
+            customer: customer.id,
+        })
+
+        logger.info("Payment created successfully");
+        response.response(res, "Payment created successfully", session.url, 201);
+    } catch (error) {
+        logger.error("Error while creating payment", error);
+        return response.response(res, "Error while creating payment", null, 500);
+    }
+};
--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -3,8 +3,12 @@ const router = app.Router();
 
 const userRoutes = require('./userRoutes');
 const bookingRoutes = require('./bookingRoutes');
+const transportationRoutes = require('./transportationRoutes');
+const paymentRoutes = require('./paymentRoutes');
 
 router.use('/planetHop-user', userRoutes);
 router.use('/planetHop-booking', bookingRoutes);
+router.use('/planetHop-transportation', transportationRoutes);
+router.use('/planetHop-payment', paymentRoutes);
 
 module.exports = router;
