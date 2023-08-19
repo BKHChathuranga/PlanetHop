2938572728ab063b985129c120344b885c5a05a0
NipunaDilshan99 <122593044+NipunaDilshan99@users.noreply.github.com>
2023-08-19 17:17:57 +0530

    Completed Booking Confimation Screen

--- front-end/src/navigation.js
+++ front-end/src/navigation.js
@@ -1,6 +1,7 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import RegistrationScreen from './screens/RegistrationScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import BookingConfirmed from './screens/BookingConfirmed';
 
 const Drawer = createDrawerNavigator();
 
@@ -9,6 +10,7 @@ const NavContainer = () => {
     <Drawer.Navigator>
       <Drawer.Screen name="Register" component={RegistrationScreen} />
       <Drawer.Screen name="Profile" component={ProfileScreen} />
+      <Drawer.Screen name="BookingConfirmed" component={BookingConfirmed} />
     </Drawer.Navigator>
   );
 }
