c28454a14cd538887d6abd3adaa7d974bfa209be
Thilina1999 <w.thilinamalshanfernando@gmail.com>
2023-08-20 12:06:46 +0530

    backend: add email

--- back-end/utils/emailConfirmation.js
+++ back-end/utils/emailConfirmation.js
@@ -13,7 +13,7 @@ exports.sendConfirmationEmail = async (detail) => {
 
     message = {
       from: process.env.USER_EMAIL,
-      to: "wildwolf1999warzone@gmail.com",
+      to: detail.email,
       subject: "Order Confirmation",
       html: `<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
       <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
